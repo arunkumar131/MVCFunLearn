5c91156d28cf5561d34b3cfea1c8e5b2e2158576
Arun Kumar <arun.kumar@klm.com>
2019-04-19 14:03:05 +0200

    Developed the new Add/Update Form in Customer

--- MVCFunLearn/Controllers/CustomersController.cs
+++ MVCFunLearn/Controllers/CustomersController.cs
@@ -41,6 +41,53 @@ namespace MVCFunLearn.Controllers
 
             return View(customer);
         }
+        
+        //Display the New customer page
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.Name = customer.Name;
+                customerInDb.BirthDate = customer.BirthDate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+
+                //Another way to update the data
+                //TryUpdateModel(customerInDb);
+            }
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+            var viewModel = new NewCustomerViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+            return View("CustomerForm", viewModel);
+        }
 
         #region Hard Code used initiallly
         //private IEnumerable<Customer> GetCustomers()
---
+++ MVCFunLearn/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVCFunLearn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCFunLearn.ViewModels
+{
+    public class NewCustomerViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
