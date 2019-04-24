59a788dcb5d4b69d45e64c82b94d346f773469bb
Arun Kumar <arun.kumar@klm.com>
2019-04-24 14:41:02 +0200

    Implemented Jquery and consume the Delete API Added the Bootbox.js  in scripts

--- MVCFunLearn/App_Start/BundleConfig.cs
+++ MVCFunLearn/App_Start/BundleConfig.cs
@@ -20,7 +20,9 @@ namespace MVCFunLearn
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootbox.js",
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
